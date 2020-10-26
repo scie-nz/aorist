e9a949426320f6965d0b6541e151d68aee65cc82
Bogdan State <bogdanstate@gmail.com>
2020-10-26 15:56:42 +1300

    Added Presto type annotation macros

---
+++ aorist_derive/src/lib.rs
@@ -0,0 +1,46 @@
+extern crate proc_macro;
+use quote::quote;
+use syn;
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(PrestoVarchar)]
+pub fn derive_presto_varchar(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+	let gen = quote! {
+        impl TPrestoAttribute for #name {
+            fn get_presto_type(&self) -> String {
+				"VARCHAR".to_string()
+            }
+        }
+    };
+	gen.into()
+}
+
+#[proc_macro_derive(PrestoBigint)]
+pub fn derive_presto_bigint(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+	let gen = quote! {
+        impl TPrestoAttribute for #name {
+            fn get_presto_type(&self) -> String {
+				"BIGINT".to_string()
+            }
+        }
+    };
+	gen.into()
+}
+
+#[proc_macro_derive(PrestoReal)]
+pub fn derive_presto_real(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+	let gen = quote! {
+        impl TPrestoAttribute for #name {
+            fn get_presto_type(&self) -> String {
+				"REAL".to_string()
+            }
+        }
+    };
+	gen.into()
+}
