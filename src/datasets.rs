d878d06ec2db0d6c4fc91876d8cc8ce3c97f8f41
Bogdan State <bogdanstate@gmail.com>
2020-10-27 11:32:14 +1300

    Added User

--- src/datasets.rs
+++ src/datasets.rs
@@ -13,27 +13,65 @@ pub struct DataSet {
     datumTemplates: Vec<DatumTemplate>,
     assets: Vec<Asset>,
 }
+impl DataSet {
+    pub fn to_yaml(&self) -> String {
+        serde_yaml::to_string(self).unwrap()
+    }
+    pub fn get_presto_schemas(&self) -> Option<String> {
+        Some(self.datumTemplates[0].get_presto_schema())
+    }
+}
 
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct User {
+    name: String,
+    email: String,
+    phone: String,
+}
+impl User {
+    pub fn to_yaml(&self) -> String {
+        serde_yaml::to_string(self).unwrap()
+    }
+}
 #[derive(Serialize, Deserialize)]
 #[serde(tag = "type", content = "spec")]
 pub enum Object {
     DataSet(DataSet),
+    User(User),
 }
 impl Object {
     pub fn to_yaml(&self) -> String {
         match self {
             Object::DataSet{..} => serde_yaml::to_string(self).unwrap(),
-        }
-    }
-    pub fn get_presto_schemas(&self) -> String {
-        match self {
-            Object::DataSet(x) => x.datumTemplates[0].get_presto_schema(),
+            Object::User{..} => serde_yaml::to_string(self).unwrap(),
         }
     }
 }
-
-pub fn get_dataset() -> Object {
+pub struct DataSetup {
+    users: Vec<User>,
+    datasets: Vec<DataSet>,
+}
+impl DataSetup {
+    pub fn get_users(&self) -> &Vec<User> {
+        &self.users
+    }
+    pub fn get_datasets(&self) -> &Vec<DataSet> {
+        &self.datasets
+    }
+}
+pub fn get_data_setup() -> DataSetup {
     let s = fs::read_to_string("basic.yaml").unwrap();
-    let dataset: Object = serde_yaml::from_str(&s).unwrap();
-    dataset
+    let objects: Vec<Object> = s
+        .split("\n---\n")
+        .filter(|x| x.len() > 0)
+        .map(|x| serde_yaml::from_str(x).unwrap())
+        .collect();
+    let mut dataSetup = DataSetup{ users: Vec::new(), datasets: Vec::new() };
+    for object in objects {
+        match object {
+            Object::User(u) => dataSetup.users.push(u),
+            Object::DataSet(d) => dataSetup.datasets.push(d),
+        }
+    }
+    dataSetup
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use lib::datasets::get_dataset;
+use lib::datasets::get_data_setup;
 
 fn main() -> Result<(), String> {
-    let dataset = get_dataset();
-    println!("{}", dataset.to_yaml());
-    println!("{}", dataset.get_presto_schemas());
+    let setup = get_data_setup();
+    for dataset in setup.get_datasets() {
+        println!("{}", dataset.to_yaml());
+        println!("{}", dataset.get_presto_schemas().unwrap());
+    }
+    for user in setup.get_users() {
+        println!("{}", user.to_yaml());
+    }
     Ok(())
 }
