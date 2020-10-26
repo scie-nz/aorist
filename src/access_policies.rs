fcc660242968664baa99e12af5711c4ab248a1f1
Bogdan State <bogdanstate@gmail.com>
2020-10-26 13:57:13 +1300

    Added struct as enum type for ApproveAccessSelector

--- src/access_policies.rs
+++ src/access_policies.rs
@@ -3,8 +3,13 @@ use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct ApproveAccessSelector {
+    matchLabels: HashMap<String, Vec<String>>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 #[serde(tag = "type", content = "spec")]
 pub enum AccessPolicy {
-    ApproveAccessSelector { matchLabels: HashMap<String, Vec<String>> },
+    ApproveAccessSelector(ApproveAccessSelector),
 }
 
