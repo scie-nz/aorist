32a5d0ade2f821dc39df5da0160c55bf0d4dc890
Bogdan State <bogdanstate@gmail.com>
2020-11-04 11:50:58 +1300

    Refactored get_data_setup

--- src/datasets.rs
+++ src/datasets.rs
@@ -55,7 +55,7 @@ impl DataSet {
     }
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct DataSetup {
     name: String,
     users: Vec<String>,
@@ -161,50 +161,10 @@ pub fn get_data_setup() -> ParsedDataSetup {
         .filter(|x| x.len() > 0)
         .map(|x| serde_yaml::from_str(x).unwrap())
         .collect();
-    let mut dataSetup = ParsedDataSetup{
-        name: "".to_string(),
-        users: None,
-        datasets: None,
-        groups: None,
-        role_bindings: None,
-    };
-
-    let mut users: Vec<User> = Vec::new();
-    let mut groups: Vec<UserGroup> = Vec::new();
-    let mut datasets: Vec<DataSet> = Vec::new();
-    let mut role_bindings: Vec<RoleBinding> = Vec::new();
-
-    for object in objects {
-        match object {
-            AoristObject::User(u) => users.push(u),
-            AoristObject::DataSet(d) => datasets.push(d),
-            AoristObject::UserGroup(g) => groups.push(g),
-            AoristObject::RoleBinding(r) => role_bindings.push(r),
-            _ => {}
-        }
-    }
-    dataSetup.set_users(users).unwrap();
-    dataSetup.set_groups(groups).unwrap();
-    dataSetup.set_datasets(datasets).unwrap();
-    dataSetup.set_role_bindings(role_bindings).unwrap();
-
-    let mut role_map: HashMap<String, Vec<Role>> = HashMap::new();
-    for binding in dataSetup.get_role_bindings().unwrap() {
-        role_map.entry(binding.get_user_name().clone()).or_insert_with(Vec::new).push(binding.get_role().clone());
-    }
-    let mut user_map: HashMap<String, &mut User> = HashMap::new();
-
-    for user in dataSetup.get_users_mut().unwrap().iter_mut() {
-        let username = user.get_unixname();
-        if role_map.contains_key(username) {
-            user_map.insert(username.clone(), user);
-        } else {
-            user.set_roles(Vec::new()).unwrap();
-        }
-    }
-    for (user_name, roles) in role_map.into_iter() {
-        user_map.get_mut(&user_name).unwrap().set_roles(roles).unwrap();
-    }
-    //dataSetup.set_optional_assets(dataSetup.datasets.iter().clone().collect());
-    dataSetup
+    let v: Vec<Option<&DataSetup>> = objects.iter().map(|x| match x {
+        AoristObject::DataSetup(x) => Some(x),
+        _ => None
+    }).filter(|x| x.is_some()).collect();
+    let dataSetup: DataSetup = v.first().unwrap().unwrap().to_owned();
+    dataSetup.parse(objects)
 }
