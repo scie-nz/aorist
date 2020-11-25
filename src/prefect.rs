2abbafffac898a1fe6e03106dd7525503a9b71f4
Bogdan State <bogdan@scie.nz>
2020-11-25 23:44:51 +1300

    Added PrefectProgram struct

--- src/prefect.rs
+++ src/prefect.rs
@@ -9,6 +9,56 @@ use crate::schema::DataSchema;
 use crate::templates::DatumTemplate;
 use enum_dispatch::enum_dispatch;
 use std::collections::HashMap;
+use rustpython_parser::ast::{Location, Located, Program, Statement, StatementType, WithItem, ExpressionType};
+
+struct PrefectProgram {
+    imports: Vec<Statement>,
+    preamble: Vec<Statement>,
+    flow: Vec<Statement>,
+}
+impl PrefectProgram {
+    fn empty() -> Self {
+        Self{ imports: Vec::new(), preamble: Vec::new(), flow: Vec::new() } 
+    }
+    fn program(self) -> Program {
+        let mut statements = self.imports;
+        for elem in self.preamble {
+            statements.push(elem);
+        }
+        let mut loc = statements.iter().last().unwrap().location;
+        loc.newline();
+        let with = StatementType::With{
+            is_async: false,
+            items: vec![
+                WithItem {
+                    context_expr: Located {
+                        location: loc,
+                        node: ExpressionType::NamedExpression{
+                            // TODO: change this to Flow(...)
+                            left: Box::new(Located{
+                                location: loc,
+                                node: ExpressionType::Identifier{name: "flow".into()}
+                            }),
+                            right: Box::new(Located{
+                                location: loc,
+                                node: ExpressionType::Identifier{name: "flow".into()}
+                            })
+                        }
+                    },
+                    optional_vars: None,
+                }
+            ],
+            body: self.flow
+        };
+        statements.push(
+            Located {
+                location: loc,
+                node: with,
+            }
+        );
+        Program{ statements }
+    }
+}
 
 #[enum_dispatch(
     RemoteWebsiteLocation,
