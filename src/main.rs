c67aa142484599154656c69ee39bd6fd54c8f0e4
Bogdan State <bogdan@scie.nz>
2020-11-27 19:11:47 +1300

    build script example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use lib::utils::get_data_setup;
+include!(concat!(env!("OUT_DIR"), "/hello.rs"));
 
 fn main() -> Result<(), String> {
+    let _foo = Foo{one: 1}; 
     let setup = get_data_setup();
     /*for dataset in setup.get_datasets().unwrap() {
         println!("{}", dataset.to_yaml());
--- src/prefect.rs
+++ src/prefect.rs
@@ -8,8 +8,10 @@ use crate::python::TObjectWithPythonCodeGen;
 use crate::schema::DataSchema;
 use crate::templates::DatumTemplate;
 use enum_dispatch::enum_dispatch;
+use rustpython_parser::ast::{
+    ExpressionType, Located, Location, Program, Statement, StatementType, WithItem,
+};
 use std::collections::HashMap;
-use rustpython_parser::ast::{Location, Located, Program, Statement, StatementType, WithItem, ExpressionType};
 
 struct PrefectProgram {
     imports: Vec<Statement>,
@@ -18,7 +20,11 @@ struct PrefectProgram {
 }
 impl PrefectProgram {
     fn empty() -> Self {
-        Self{ imports: Vec::new(), preamble: Vec::new(), flow: Vec::new() } 
+        Self {
+            imports: Vec::new(),
+            preamble: Vec::new(),
+            flow: Vec::new(),
+        }
     }
     fn program(self) -> Program {
         let mut statements = self.imports;
@@ -27,36 +33,36 @@ impl PrefectProgram {
         }
         let mut loc = statements.iter().last().unwrap().location;
         loc.newline();
-        let with = StatementType::With{
+        let with = StatementType::With {
             is_async: false,
-            items: vec![
-                WithItem {
-                    context_expr: Located {
-                        location: loc,
-                        node: ExpressionType::NamedExpression{
-                            // TODO: change this to Flow(...)
-                            left: Box::new(Located{
-                                location: loc,
-                                node: ExpressionType::Identifier{name: "flow".into()}
-                            }),
-                            right: Box::new(Located{
-                                location: loc,
-                                node: ExpressionType::Identifier{name: "flow".into()}
-                            })
-                        }
+            items: vec![WithItem {
+                context_expr: Located {
+                    location: loc,
+                    node: ExpressionType::NamedExpression {
+                        // TODO: change this to Flow(...)
+                        left: Box::new(Located {
+                            location: loc,
+                            node: ExpressionType::Identifier {
+                                name: "flow".into(),
+                            },
+                        }),
+                        right: Box::new(Located {
+                            location: loc,
+                            node: ExpressionType::Identifier {
+                                name: "flow".into(),
+                            },
+                        }),
                     },
-                    optional_vars: None,
-                }
-            ],
-            body: self.flow
+                },
+                optional_vars: None,
+            }],
+            body: self.flow,
         };
-        statements.push(
-            Located {
-                location: loc,
-                node: with,
-            }
-        );
-        Program{ statements }
+        statements.push(Located {
+            location: loc,
+            node: with,
+        });
+        Program { statements }
     }
 }
 
