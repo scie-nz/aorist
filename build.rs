75f69f14d52ccfcb41a69c7409cfcec615988e21
Bogdan State <bogdan@scie.nz>
2020-11-27 19:52:50 +1300

    Reading basic yaml in build script

---
+++ build.rs
@@ -0,0 +1,29 @@
+use codegen::Scope;
+use std::env;
+use std::fs;
+use std::path::Path;
+use serde_yaml::{from_str, Value};
+use std::collections::HashMap;
+
+pub fn read_file(filename: &str) -> Vec<HashMap<String, Value>> {
+    let s = fs::read_to_string(filename).unwrap();
+    s
+        .split("\n---\n")
+        .filter(|x| x.len() > 0)
+        .map(|x| from_str(x).unwrap())
+        .collect()
+}
+
+fn main() {
+	let raw_objects = read_file("basic.yaml");
+
+  let out_dir = env::var_os("OUT_DIR").unwrap();
+  let dest_path = Path::new(&out_dir).join("hello.rs");    
+	
+	let mut scope = Scope::new();
+    scope.new_struct("Foo")
+        .derive("Debug")
+        .field("one", "usize");
+    fs::write(&dest_path, scope.to_string());
+    println!("cargo:rerun-if-changed=build.rs");
+}
