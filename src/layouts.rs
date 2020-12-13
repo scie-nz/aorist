07b52f94a952c423ec047cff5615a4c8c7e1ac1c
Bogdan State <bogdan@scie.nz>
2020-12-14 09:37:56 +1300

    Defined DailyGranularity and DynamicHiveTableLayout

--- src/layouts.rs
+++ src/layouts.rs
@@ -15,7 +15,22 @@ pub enum StorageLayout {
 pub struct StaticHiveTableLayout {}
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+pub struct DailyGranularity {}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+#[serde(tag = "type")]
+pub enum Granularity {
+    DailyGranularity(DailyGranularity),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+pub struct DynamicHiveTableLayout {
+    granularity: DailyGranularity,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 #[serde(tag = "type")]
 pub enum HiveStorageLayout {
     StaticHiveTableLayout(StaticHiveTableLayout),
+    DynamicHiveTableLayout(DynamicHiveTableLayout),
 }
