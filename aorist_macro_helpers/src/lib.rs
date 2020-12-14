c7b38c6e78bfe4f346c1991982f3147cf22ccb98
Bogdan State <bogdan@scie.nz>
2020-12-14 22:02:17 +1300

    Added extract_type_from_vector to aorist_macro_helpers

--- aorist_macro_helpers/src/lib.rs
+++ aorist_macro_helpers/src/lib.rs
@@ -2,7 +2,8 @@ use syn;
 
 // https://stackoverflow.com/questions/55271857/how-can-i-get-the-t-from-an-optiont-when-using-syn/55277337
 // https://rust-syndication.github.io/rss/src/derive_builder_core/setter.rs.html#198
-pub fn extract_type_from_option(ty: &syn::Type) -> Option<&syn::Type> {
+fn extract_inner_from_bracketed_type<'a>(ty: &'a syn::Type, idents:
+Vec<String>) -> Option<&'a syn::Type> {
     use syn::punctuated::Pair;
     use syn::token::Colon2;
     use syn::{GenericArgument, Path, PathArguments, PathSegment};
@@ -16,7 +17,8 @@ pub fn extract_type_from_option(ty: &syn::Type) -> Option<&syn::Type> {
 
     // TODO store (with lazy static) the vec of string
     // TODO maybe optimization, reverse the order of segments
-    fn extract_option_segment(path: &Path) -> Option<Pair<&PathSegment, &Colon2>> {
+    let extract_option_segment = |path: &'a Path| -> Option<Pair<&'a
+    PathSegment, &'a Colon2>> {
         let idents_of_path = path
             .segments
             .iter()
@@ -26,14 +28,15 @@ pub fn extract_type_from_option(ty: &syn::Type) -> Option<&syn::Type> {
                 acc.push('|');
                 acc
             });
-        vec!["Option|", "std|option|Option|", "core|option|Option|"]
+            idents
             .into_iter()
-            .find(|s| &idents_of_path == *s)
+            .find(|s| idents_of_path == *s)
             .and_then(|_| path.segments.last()).map(Pair::End)
-    }
+    };
 
     let tp = extract_type_path(&ty);
-    let tp2 = tp.and_then(|path| extract_option_segment(path));
+    let tp2 = tp.and_then(|path| extract_option_segment(path, 
+    ));
     let tp3 = tp2.and_then(|pair_path_segment| {
             let type_params = &pair_path_segment.into_value().arguments;
             // It should have only on angle-bracketed param ("<String>"):
@@ -49,3 +52,23 @@ pub fn extract_type_from_option(ty: &syn::Type) -> Option<&syn::Type> {
 }
 
 
+pub fn extract_type_from_option(ty: &syn::Type) -> Option<&syn::Type> {
+    extract_inner_from_bracketed_type(
+        ty,
+        vec![
+            "Option|".to_string(),
+            "std|option|Option|".into(),
+            "core|option|Option|".into()
+        ],
+    )
+}
+
+pub fn extract_type_from_vector(ty: &syn::Type) -> Option<&syn::Type> {
+    extract_inner_from_bracketed_type(
+        ty,
+        vec![
+            "Vector|".to_string(),
+            "std|vec|Vec|".into(),
+        ],
+    )
+}
