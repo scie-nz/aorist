e36a0c0bdb3d8d6652bb41417539151ab6600bb3
Bogdan State <bogdan@scie.nz>
2020-12-19 13:19:22 +1300

    Added get_uuid_from_children_uuid on AoristConcept

--- src/concept/mod.rs
+++ src/concept/mod.rs
@@ -1,4 +1,7 @@
 use crate::constraint::Constraint;
+use siphasher::sip128::{Hasher128, SipHasher};
+use std::collections::BTreeSet;
+use std::hash::Hasher;
 use std::rc::Rc;
 use uuid::Uuid;
 
@@ -11,4 +14,22 @@ pub trait AoristConcept {
     fn traverse_constrainable_children(&self, upstream_constraints: Vec<Rc<Constraint>>);
     fn get_uuid(&self) -> Uuid;
     fn get_children_uuid(&self) -> Vec<Uuid>;
+
+    fn get_uuid_from_children_uuid(&self) -> Uuid {
+        let child_uuids = self.get_children_uuid();
+        if child_uuids.len() > 0 {
+            let uuids = child_uuids
+                .into_iter()
+                .collect::<BTreeSet<Uuid>>();
+            let mut hasher = SipHasher::new();
+            for uuid in uuids {
+                hasher.write(uuid.as_bytes());
+            }
+            let bytes: [u8; 16] = hasher.finish128().as_bytes();
+            Uuid::from_bytes(bytes)
+        } else {
+            // TODO: this should just be created from the hash
+            Uuid::new_v4()
+        }
+    }
 }
