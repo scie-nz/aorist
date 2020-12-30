63c3bd8e811418420fb78b4a07ab364bf8dcd87b
Bogdan State <bogdan@scie.nz>
2020-12-30 16:48:16 +1300

    Forgot to add TSVEncoding file

---
+++ src/encoding/tsv_encoding.rs
@@ -0,0 +1,25 @@
+#![allow(non_snake_case)]
+
+use crate::compression::DataCompression;
+use crate::concept::{AoristConcept, Concept};
+use crate::constraint::Constraint;
+use crate::header::FileHeader;
+use aorist_concept::Constrainable;
+use derivative::Derivative;
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, RwLock};
+use uuid::Uuid;
+
+#[derive(Derivative, Serialize, Deserialize, Clone, Constrainable)]
+#[derivative(PartialEq, Debug)]
+pub struct TSVEncoding {
+    #[constrainable]
+    compression: DataCompression,
+    #[constrainable]
+    header: FileHeader,
+    uuid: Option<Uuid>,
+    tag: Option<String>,
+    #[serde(skip)]
+    #[derivative(PartialEq = "ignore", Debug = "ignore")]
+    pub constraints: Vec<Arc<RwLock<Constraint>>>,
+}
