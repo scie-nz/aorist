a95cecdb89cd0c59be0dd28a51e17bcc8a91c57c
Bogdan State <bogdan@scie.nz>
2021-02-08 21:33:43 +1300

    Added setup.py + various manifests for pip source building

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    name="aorist",
+    version="0.0.1",
+    rust_extensions=[RustExtension("aorist.aorist")],
+    packages=["aorist"],
+    # Rust extensions are not zip safe
+    zip_safe=False,
+    long_description="""
+    Aorist: ETL code generation for flexible environments and infrastructure
+    """,
+    long_description_content_type="text/x-rst"
+)
