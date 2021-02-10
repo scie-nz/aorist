5225a9b19fcce49cda4a58f789c42fa2f62ffd25
Aorist Agent <hello@scie.nz>
2021-02-10 20:42:54 +0000

    Minor changes to devserver entrypoint

--- k3s/devserver/entrypoint.sh
+++ k3s/devserver/entrypoint.sh
@@ -5,3 +5,5 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone git@github.com:scie-nz/aorist.git
 git config --global user.email "hello@scie.nz"
 git config --global user.name "Aorist Agent"
+export GIT_SSH_COMMAND='ssh -i /etc/deploy_key/ssh-privatekey'
+while true; do sleep 30; done;
