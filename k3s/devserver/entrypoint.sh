3ce963e3d63d63894227c8f4010e6496f748bfed
Bogdan State <bogdan@scie.nz>
2021-02-11 10:03:06 +1300

    entrypoint for devserver compiles aorist

--- k3s/devserver/entrypoint.sh
+++ k3s/devserver/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-ssh-agent /bin/bash
+cd ~
+export GIT_SSH_COMMAND='ssh -i /etc/deploy_key/ssh-privatekey'
 ssh-add /etc/deploy_key/ssh-privatekey
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-git clone git@github.com:scie-nz/aorist.git
+mkdir -p ~/.ssh
+eval "$(ssh-agent)" && ssh-keyscan github.com >> ~/.ssh/known_hosts && git clone -q git@github.com:scie-nz/aorist.git
 git config --global user.email "hello@scie.nz"
 git config --global user.name "Aorist Agent"
-export GIT_SSH_COMMAND='ssh -i /etc/deploy_key/ssh-privatekey'
+cd aorist
+cargo build
 while true; do sleep 30; done;
+
