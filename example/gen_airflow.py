7714da34385d4f0cfb9ea8518cab85c6b8746bf8
Bogdan State <bogdan@scie.nz>
2021-02-19 17:58:51 +1300

    Added (non-working) example for ML generation

--- example/gen_airflow.py
+++ example/gen_airflow.py
@@ -6,7 +6,8 @@ from aorist import (
 from common import DEFAULT_USERS, DEFAULT_GROUPS, DEFAULT_ENDPOINTS
 from sentinel import sentinel_dataset
 from snap import snap_dataset
-from geonames import geonames_dataset
+from geonames import geonames_dataset, geonames_table
+from beers import beers_dataset, beers_table
 
 universe = Universe(
     name="my_cluster",
@@ -14,6 +15,8 @@ universe = Universe(
     groups=DEFAULT_GROUPS,
     datasets=[
         snap_dataset,
+        geonames_dataset,
+        beers_dataset,
     ],
     endpoints=DEFAULT_ENDPOINTS,
     compliance=ComplianceConfig(
@@ -28,6 +31,55 @@ universe = Universe(
         data_about_human_subjects=True,
         contains_personally_identifiable_information=False,
     ),
+    models=[
+        SingleObjectiveRegressor(
+            name="predict_beer_abv",
+            source_data=[beers_table],
+            algorithm=RandomForestRegressionAlgorithm(),
+            target=ContinuousObjective(beers_table.schema.get('abv')).log(base=2),
+            validation_config=TrainTestRegressionValidationConfig(test_ratio=0.05),
+            feature_groups=[
+                CountFeatureGroup(
+                    name="top_words_in_name",
+                    data=TextToTokenCounts(
+                        data=KeepMostFrequent(
+                            data=[
+                                Tokenize(
+                                    Lowercase(
+                                        TextFeature(beers_table.schema.get('name')),
+                                    )
+                                )
+                            ],
+                            min_frequency=100,
+                        ),
+                    ),
+                ),
+                CountFeatureGroup(
+                    name="top_words_in_style",
+                    data=TextToTokenCounts(
+                        data=KeepMostFrequent(
+                            data=[
+                                Tokenize(
+                                    Lowercase(
+                                        TextFeature(beers_table.schema.get('style')),
+                                    )
+                                )
+                            ],
+                            min_frequency=100,
+                        ),
+                    ),
+                ),
+                EmbeddingFeatureGroup(
+                    name="name_embeddings",
+                    data=FasttextEmbeddings(
+                        data=[Lowercase(
+                            TextFeature(beers_table.schema.get('style')),
+                        )]
+                    )
+                ),
+            ],
+        ),
+    ],
 )
 out = dag(universe, [
     "DataDownloadedAndConverted"
