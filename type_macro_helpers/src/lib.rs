4dbb5fee30b1c9ada5ac1bca1068b111f04e2969
Bogdan State <bogdan@scie.nz>
2021-03-03 04:09:42 +1300

    Added extract_type_from_map to type_macro_helpers

--- type_macro_helpers/src/lib.rs
+++ type_macro_helpers/src/lib.rs
@@ -1,4 +1,6 @@
-use syn::Path;
+use syn::punctuated::Pair;
+use syn::token::Colon2;
+use syn::{GenericArgument, Path, PathArguments, PathSegment};
 
 pub fn extract_type_path(ty: &syn::Type) -> Option<&Path> {
     match *ty {
@@ -7,16 +9,10 @@ pub fn extract_type_path(ty: &syn::Type) -> Option<&Path> {
     }
 }
 
-// https://stackoverflow.com/questions/55271857/how-can-i-get-the-t-from-an-optiont-when-using-syn/55277337
-// https://rust-syndication.github.io/rss/src/derive_builder_core/setter.rs.html#198
-fn extract_inner_from_bracketed_type<'a>(
+fn get_inner_type<'a>(
     ty: &'a syn::Type,
     idents: Vec<String>,
-) -> Option<&'a syn::Type> {
-    use syn::punctuated::Pair;
-    use syn::token::Colon2;
-    use syn::{GenericArgument, PathArguments, PathSegment};
-
+) -> Option<Pair<&'a PathSegment, &'a Colon2>> {
     // TODO store (with lazy static) the vec of string
     // TODO maybe optimization, reverse the order of segments
     let extract_option_segment = |path: &'a Path| -> Option<Pair<&'a PathSegment, &'a Colon2>> {
@@ -37,7 +33,16 @@ fn extract_inner_from_bracketed_type<'a>(
     };
 
     let tp = extract_type_path(&ty);
-    let tp2 = tp.and_then(|path| extract_option_segment(path));
+    tp.and_then(|path| extract_option_segment(path))
+}
+
+// https://stackoverflow.com/questions/55271857/how-can-i-get-the-t-from-an-optiont-when-using-syn/55277337
+// https://rust-syndication.github.io/rss/src/derive_builder_core/setter.rs.html#198
+fn extract_inner_from_bracketed_type<'a>(
+    ty: &'a syn::Type,
+    idents: Vec<String>,
+) -> Option<&'a syn::Type> {
+    let tp2 = get_inner_type(ty, idents);
     let tp3 = tp2.and_then(|pair_path_segment| {
         let type_params = &pair_path_segment.into_value().arguments;
         // It should have only on angle-bracketed param ("<String>"):
@@ -48,6 +53,31 @@ fn extract_inner_from_bracketed_type<'a>(
     });
     tp3.and_then(|generic_arg| match *generic_arg {
         GenericArgument::Type(ref ty) => Some(ty),
+        _ => None,
+    })
+}
+
+fn extract_inner_from_double_bracketed_type<'a>(
+    ty: &'a syn::Type,
+    idents: Vec<String>,
+) -> Option<(&'a syn::Type, &'a syn::Type)> {
+    let tp2 = get_inner_type(ty, idents);
+    let tp3 = tp2.and_then(|pair_path_segment| {
+        let type_params = &pair_path_segment.into_value().arguments;
+        // It should have only on angle-bracketed param ("<String>"):
+        match *type_params {
+            PathArguments::AngleBracketed(ref params) => {
+                assert_eq!(params.args.len(), 2);
+                let mut it = params.args.iter();
+                let first = it.next().unwrap();
+                let second = it.next().unwrap();
+                Some((first, second))
+            }
+            _ => None,
+        }
+    });
+    tp3.and_then(|generic_arg| match generic_arg {
+        (&GenericArgument::Type(ref ty1), &GenericArgument::Type(ref ty2)) => Some((ty1, ty2)),
         _ => None,
     })
 }
@@ -70,6 +100,18 @@ pub fn extract_type_from_vector(ty: &syn::Type) -> Option<&syn::Type> {
             "Vector|".to_string(),
             "std|vec|Vec|".into(),
             "Vec|".to_string(),
+        ],
+    )
+}
+
+pub fn extract_type_from_map(ty: &syn::Type) -> Option<(&syn::Type, &syn::Type)> {
+    extract_inner_from_double_bracketed_type(
+        ty,
+        vec![
+            "BTreeMap|".to_string(),
+            "std|collections|BTreeMap|".into(),
+            "HashMap|".to_string(),
+            "std|collections|HashMap|".into(),
         ],
     )
 }
