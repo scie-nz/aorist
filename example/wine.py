01abf02c42f165d262865937b9aced567a0fd6ec
Bogdan State <bogdan@scie.nz>
2021-03-03 12:27:38 +1300

    Added more comments

--- example/wine.py
+++ example/wine.py
@@ -189,12 +189,15 @@ wine_dataset = DataSet(
     assets={"wine_table": wine_table},
 )
 
+# We will train a classifier and store it in a local file.
 classifier_storage = LocalFileStorage(
     location=MinioLocation(name="wine"),
     layout=SingleFileLayout(),
     encoding=ONNXEncoding(),
 )
+# We will use these as the features in our classifier.
 features = attributes[2:10]
+# This is the "recipe" for our classifier.
 classifier_template = TrainedFloatMeasure(
     name="predicted_alcohol",
     comment="""
@@ -205,12 +208,19 @@ classifier_template = TrainedFloatMeasure(
     objective=attributes[1],
     source_asset_name="wine_table",
 )
+# We now augment the dataset with this recipe.
 wine_dataset.add_template(classifier_template)
+# The classifier is computed from local data
+# (note the source_asset_names dictionary)
 classifier_setup = ComputedFromLocalData(
     source_asset_names={"training_dataset": "wine_table"},
     target=classifier_storage,
     tmp_dir="/tmp/wine_classifier",
 )
+# We finally define our regression_model as a concrete
+# data asset, following a recipe defined by the template,
+# while also connected to concrete storage, as defined
+# by classifier_setup
 regression_model = SupervisedModel(
     name="wine_alcohol_predictor",
     tag="predictor",
@@ -221,14 +231,23 @@ regression_model = SupervisedModel(
 )
 wine_dataset.add_asset(regression_model)
 
-predictions_template = classifier_template.as_predictions_template(name="alcohol_predictions")
+
+# We now prepare to make predictions based on the classifier
+# we just trained. The abstract recipe for this is a predictions
+# template (this assumes a trained model and an input data asset
+# with a compatible schema for feature requirements).
+predictions_template = classifier_template.as_predictions_template(
+    name="alcohol_predictions"
+)
 wine_dataset.add_template(predictions_template)
 
+# We will store our predictions in a Hive table
 predictions_storage = HiveTableStorage(
     location=MinioLocation(name="wine"),
     layout=StaticHiveTableLayout(),
     encoding=ORCEncoding(),
 )
+# Our predictions will be computed from local data
 predictions_setup = ComputedFromLocalData(
     source_asset_names={
         "model": "wine_alcohol_predictor",
@@ -237,6 +256,9 @@ predictions_setup = ComputedFromLocalData(
     target=predictions_storage,
     tmp_dir="/tmp/wine_predictions",
 )
+# Our predictions will be available as a static data
+# table (which will also include the features).
+# Note: the schema has columns in a specific order.
 predictions_table = StaticDataTable(
     tag="wine_predictions",
     name="wine_predictions",
