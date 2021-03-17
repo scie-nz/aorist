4e7801e1cf100a77ccd13a9616ea914492cf79ab
Bogdan State <bogdan@scie.nz>
2021-03-17 13:09:09 +1300

    Added most common Postgres types

--- aorist_derive/src/lib.rs
+++ aorist_derive/src/lib.rs
@@ -198,3 +198,283 @@ pub fn derive_sqlite_text(input: TokenStream) -> TokenStream {
     };
     gen.into()
 }
+
+#[proc_macro_derive(PostgresSmallInt)]
+pub fn derive_postgres_smallint(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "SMALLINT".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresInteger)]
+pub fn derive_postgres_integer(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "INTEGER".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresBigInt)]
+pub fn derive_postgres_bigint(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "BIGINT".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresDecimal)]
+pub fn derive_postgres_decimal(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "DECIMAL".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresNumeric)]
+pub fn derive_postgres_numeric(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "NUMERIC".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresReal)]
+pub fn derive_postgres_real(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "REAL".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresDoublePrecision)]
+pub fn derive_postgres_doubleprecision(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "DOUBLE PRECISION".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresSmallSerial)]
+pub fn derive_postgres_smallserial(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "SMALLSERIAL".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresSerial)]
+pub fn derive_postgres_serial(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "SERIAL".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresBigSerial)]
+pub fn derive_postgres_bigserial(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "BIGSERIAL".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresMoney)]
+pub fn derive_postgres_money(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "MONEY".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresVarchar)]
+pub fn derive_postgres_varchar(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "VARCHAR".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresChar)]
+pub fn derive_postgres_char(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "CHAR".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresText)]
+pub fn derive_postgres_text(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "TEXT".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresBytea)]
+pub fn derive_postgres_bytea(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "BYTEA".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresTimestamp)]
+pub fn derive_postgres_timestamp(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "TIMESTAMP".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresDate)]
+pub fn derive_postgres_date(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "DATE".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresTime)]
+pub fn derive_postgres_time(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "TIME".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresInterval)]
+pub fn derive_postgres_interval(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "INTERVAL".to_string()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(PostgresBoolean)]
+pub fn derive_postgres_boolean(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let gen = quote! {
+        impl TPostgresAttribute for #name {
+            fn get_postgres_type(&self) -> String {
+                "BOOLEAN".to_string()
+            }
+        }
+    };
+    gen.into()
+}
