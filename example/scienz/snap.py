8033f70e7e50de3edf6d36611481bc468a3b4e44
Bogdan State <bogdan@scie.nz>
2021-04-12 19:22:45 +1200

    Fixed SNAP

--- example/scienz/snap.py
+++ example/scienz/snap.py
@@ -28,19 +28,19 @@ edge_tuple = IdentifierTuple(
     ]),
 )
 names = [
-    "ca-AstroPh",
-    "ca-CondMat",
-    "ca-GrQc",
-    "ca-HepPh",
-    "ca-HepTh",
+    #"ca-AstroPh",
+    #"ca-CondMat",
+    #"ca-GrQc",
+    #"ca-HepPh",
+    #"ca-HepTh",
     "web-BerkStan",
-    "web-Google",
-    "web-NotreDame",
-    "web-Stanford",
-    "amazon0302",
-    "amazon0312",
-    "amazon0505",
-    "amazon0601",
+    #"web-Google",
+    #"web-NotreDame",
+    #"web-Stanford",
+    #"amazon0302",
+    #"amazon0312",
+    #"amazon0505",
+    #"amazon0601",
 ]
 tables = {}
 for name in names:
