b35d5bfdb5b28581068b6ca5624f83c343344501
Bogdan State <bogdan@scie.nz>
2021-04-29 23:40:19 +1200

    Added StandaloneRBasedTask

--- src/r/mod.rs
+++ src/r/mod.rs
@@ -1,5 +1,7 @@
 mod preamble;
 mod r_import;
+mod task;
 
 pub use preamble::RPreamble;
 pub use r_import::RImport;
+pub use task::StandaloneRBasedTask;
---
+++ src/r/task.rs
@@ -0,0 +1,50 @@
+use crate::dialect::Dialect;
+use crate::flow::{ETLFlow, StandaloneTask};
+use crate::parameter_tuple::ParameterTuple;
+use crate::python::AST;
+use std::hash::Hash;
+use std::marker::PhantomData;
+
+#[derive(Clone, Hash, PartialEq, Eq)]
+pub struct StandaloneRBasedTask<T>
+where
+    T: ETLFlow,
+{
+    /// unique task identifier
+    task_id: String,
+    /// function called to create task (has different meaning depending on
+    /// the render we use.
+    call: Option<String>,
+    /// arguments passed to function call
+    params: Option<ParameterTuple>,
+    /// R preamble used by this task call
+    preamble: Option<String>,
+    /// Dialect (e.g. Bash, R, R, Presto, etc.), to be interpreted
+    /// by render.
+    dialect: Option<Dialect>,
+    singleton_type: PhantomData<T>,
+}
+
+impl<T> StandaloneTask<T> for StandaloneRBasedTask<T>
+where
+    T: ETLFlow,
+{
+    fn new(
+        task_id: String,
+        _task_val: AST,
+        call: Option<String>,
+        params: Option<ParameterTuple>,
+        _dependencies: Vec<AST>,
+        preamble: Option<String>,
+        dialect: Option<Dialect>,
+    ) -> Self {
+        Self {
+            task_id,
+            call,
+            params,
+            preamble,
+            dialect,
+            singleton_type: PhantomData,
+        }
+    }
+}
