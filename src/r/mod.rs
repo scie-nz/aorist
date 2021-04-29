fd3ab6673c0a43c9d35ade7790791d1fbaebaf24
Bogdan State <bogdan@scie.nz>
2021-04-29 19:06:46 +1200

    Added RImport class

--- src/r/mod.rs
+++ src/r/mod.rs
@@ -1,3 +1,5 @@
+mod r_import;
 mod preamble;
 
 pub use preamble::RPreamble;
+pub use r_import::RImport;
---
+++ src/r/r_import.rs
@@ -0,0 +1,11 @@
+use crate::code::Import;
+use std::hash::Hash;
+
+#[derive(Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct RImport{ library: String }
+impl Import for RImport {}
+impl RImport {
+    pub fn new(library: String) -> Self {
+        Self { library }
+    }
+}
