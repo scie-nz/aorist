aa695d42af619404a0bfecde819c22c6eda48718
Bogdan State <bogdan@scie.nz>
2021-05-01 12:41:09 +1200

    Added NativeRTask

--- src/r/mod.rs
+++ src/r/mod.rs
@@ -2,7 +2,9 @@ mod code_block;
 mod preamble;
 mod r_import;
 mod task;
+mod native_r_task;
 
 pub use preamble::RPreamble;
 pub use r_import::RImport;
 pub use task::StandaloneRBasedTask;
+pub use native_r_task::NativeRTask;
---
+++ src/r/native_r_task.rs
@@ -0,0 +1,18 @@
+use crate::python::AST;
+use crate::r::r_import::RImport;
+use aorist_primitives::define_task_node;
+use std::hash::Hash;
+use std::sync::{Arc, RwLock};
+
+define_task_node!(
+    NativeRTask,
+    |task: &NativeRTask| task.statements.clone(),
+    |task: &NativeRTask| {
+        task.statements.clone()
+    },
+    |task: &NativeRTask| task.imports.clone(),
+    RImport,
+    statements: Vec<AST>,
+    imports: Vec<RImport>,
+    task_val: AST,
+);
