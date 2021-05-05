c8d514fc1d3f55e76bbfd28650e9b11dacd333c8
Bogdan State <bogdan@scie.nz>
2021-05-05 18:04:25 +1200

    get_statements method in PythonBasedConstraintBlock no longer depends on any struct fields

--- src/constraint_block/mod.rs
+++ src/constraint_block/mod.rs
@@ -4,7 +4,7 @@ use crate::flow::{ETLFlow, FlowBuilderInput};
 use crate::parameter_tuple::ParameterTuple;
 use crate::python::PythonBasedCodeBlock;
 use crate::python::{
-    Assignment, Dict, PythonFlowBuilderInput, PythonImport, PythonPreamble, AST,
+    Assignment, Dict, PythonFlowBuilderInput, AST,
 };
 use linked_hash_map::LinkedHashMap;
 use linked_hash_set::LinkedHashSet;
@@ -21,7 +21,12 @@ where
     type C: CodeBlock<T>;
     type BuilderInputType;
 
+    fn get_constraint_name(&self) -> String;
+    fn get_constraint_title(&self) -> Option<String>;
+    fn get_constraint_body(&self) -> Option<String>;
+    fn get_code_blocks(&'a self) -> &'a Vec<Self::C>;
     fn get_statements(&'a self, endpoints: &EndpointConfig) -> Self::BuilderInputType;
+
     fn get_identifiers(&self) -> HashMap<Uuid, AST>;
     fn new(
         constraint_name: String,
@@ -30,6 +35,7 @@ where
         members: Vec<Self::C>,
         tasks_dict: Option<AST>,
     ) -> Self;
+
 }
 
 pub struct PythonBasedConstraintBlock<T>
@@ -50,6 +56,47 @@ where
     type C = PythonBasedCodeBlock<T>;
     type BuilderInputType = PythonFlowBuilderInput;
 
+    fn get_constraint_name(&self) -> String {
+        self.constraint_name.clone()
+    }
+    fn get_constraint_title(&self) -> Option<String> {
+        self.title.clone()
+    }
+    fn get_constraint_body(&self) -> Option<String> {
+        self.body.clone()
+    }
+    fn get_code_blocks(&'a self) -> &'a Vec<PythonBasedCodeBlock<T>> {
+        &self.members
+    }
+    fn get_statements(&'a self, endpoints: &EndpointConfig) -> Self::BuilderInputType {
+        let preambles_and_statements = self
+            .get_code_blocks()
+            .iter()
+            .map(|x| x.get_statements(endpoints))
+            .collect::<Vec<_>>();
+        let preambles = preambles_and_statements
+            .iter()
+            .map(|x| x.1.clone().into_iter())
+            .flatten()
+            .collect::<LinkedHashSet<_>>();
+        let imports = preambles_and_statements
+            .iter()
+            .map(|x| x.2.clone().into_iter())
+            .flatten()
+            .collect::<BTreeSet<_>>();
+        Self::BuilderInputType::new(
+            self.get_task_val_assignments()
+                .into_iter()
+                .chain(preambles_and_statements.into_iter().map(|x| x.0).flatten())
+                .collect::<Vec<_>>(),
+            preambles,
+            imports,
+            self.get_constraint_name(),
+            self.get_constraint_title(),
+            self.get_constraint_body(),
+        )
+    }
+
     fn new(
         constraint_name: String,
         title: Option<String>,
@@ -66,35 +113,6 @@ where
             tasks_dict,
         }
     }
-
-    fn get_statements(&'a self, endpoints: &EndpointConfig) -> PythonFlowBuilderInput {
-        let preambles_and_statements = self
-            .members
-            .iter()
-            .map(|x| x.get_statements(endpoints))
-            .collect::<Vec<_>>();
-        let preambles = preambles_and_statements
-            .iter()
-            .map(|x| x.1.clone().into_iter())
-            .flatten()
-            .collect::<LinkedHashSet<PythonPreamble>>();
-        let imports = preambles_and_statements
-            .iter()
-            .map(|x| x.2.clone().into_iter())
-            .flatten()
-            .collect::<BTreeSet<PythonImport>>();
-        PythonFlowBuilderInput::new(
-            self.get_task_val_assignments()
-                .into_iter()
-                .chain(preambles_and_statements.into_iter().map(|x| x.0).flatten())
-                .collect::<Vec<_>>(),
-            preambles,
-            imports,
-            self.constraint_name.clone(),
-            self.title.clone(),
-            self.body.clone(),
-        )
-    }
     fn get_identifiers(&self) -> HashMap<Uuid, AST> {
         self.members
             .iter()
@@ -108,9 +126,6 @@ impl<'a, T> PythonBasedConstraintBlock<T>
 where
     T: ETLFlow,
 {
-    pub fn get_constraint_name(&self) -> String {
-        self.constraint_name.clone()
-    }
     pub fn get_params(&self) -> HashMap<String, Option<ParameterTuple>> {
         self.members
             .iter()
