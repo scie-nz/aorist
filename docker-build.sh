4d8dd27ac716bb8401a82422192341b5c0d6989a
Nick Parker <nick@scie.nz>
2021-05-18 16:57:48 +1200

    Try just enabling use-bindgen to fix the libR-sys CI build errors

--- docker-build.sh
+++ docker-build.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 PLATFORMS="linux/amd64,linux/arm64"
 
 IMG_TAG=$(date +%Y%m%d)
-IMG="scienz/aorist-base:${IMG_TAG}"
+IMG="scienz/aorist-build:${IMG_TAG}"
 
 echo "Building $IMG for $PLATFORMS"
 docker buildx build --push --platform $PLATFORMS -f Dockerfile.ci -t $IMG .
