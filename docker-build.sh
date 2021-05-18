3f368478679e4b41701b6476964e439f77a0ef45
Nick Parker <nick@scie.nz>
2021-05-18 12:44:45 +1200

    Define custom CI image with R preinstalled

---
+++ docker-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+PLATFORMS="linux/amd64,linux/arm64"
+
+IMG_TAG=$(date +%Y%m%d)
+IMG="scienz/aorist-base:$IMG_TAG"
+
+echo "Building $IMG for $PLATFORMS"
+docker buildx build --push --platform $PLATFORMS -f Dockerfile.ci -t $IMG .
