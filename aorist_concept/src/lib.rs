ba4d585b43d700059202ec3668c601c48e65f65c
Bogdan State <bogdan@scie.nz>
2021-05-23 11:32:36 +1200

    Introduced ConceptBuilder struct

--- aorist_concept/src/lib.rs
+++ aorist_concept/src/lib.rs
@@ -93,133 +93,136 @@ pub fn constrain_object(input: TokenStream) -> TokenStream {
     }
 }
 
-fn get_derives(attrs: Vec<NestedMeta>) -> (Vec<NestedMeta>, Vec<NestedMeta>) {
-    let mut derivatives: Vec<NestedMeta> = Vec::new();
-    let mut derives: Vec<NestedMeta> = Vec::new();
-    for attr in attrs {
-        if let NestedMeta::Meta(Meta::List(x)) = attr {
-            if x.path.is_ident("derivative") {
-                derivatives = x.nested.into_iter().collect();
-            } else if x.path.is_ident("derive") {
-                derives = x.nested.into_iter().collect();
-            } else {
-                panic!("An attribute other than derive or derivative was specified");
-            }
-        } else {
-            panic!("An attribute other than a MetaList was specified.");
-        }
-    }
-    (derives, derivatives)
-}
-
-fn add_aorist_fields(struct_data: &mut syn::DataStruct) {
-    match &mut struct_data.fields {
-        Fields::Named(fields) => {
-            fields.named.push(
-                Field::parse_named
-                    .parse2(quote! {
-                    pub uuid: Option<Uuid>
-                    })
-                    .unwrap(),
-            );
-            fields.named.push(
-                Field::parse_named
-                    .parse2(quote! {
-                    pub tag: Option<String>
-                    })
-                    .unwrap(),
-            );
-            fields.named.push(
-                Field::parse_named
-                    .parse2(quote! {
-                        #[serde(skip)]
-                        #[derivative(PartialEq = "ignore", Debug = "ignore", Hash = "ignore")]
-                        pub constraints: Vec<Arc<RwLock<Constraint>>>
-                    })
-                    .unwrap(),
-            );
-        }
-        _ => (),
-    }
-}
-
-fn extend_metas(ast: &mut DeriveInput, extra_metas: Vec<NestedMeta>, ident: &str) {
-    let (attr, mut metas) = ast
-        .attrs
-        .iter_mut()
-        .filter(|attr| attr.path.is_ident(ident))
-        .filter_map(|attr| match attr.parse_meta() {
-            Ok(Meta::List(meta_list)) => Some((attr, meta_list)),
-            _ => None, // kcov-ignore
-        })
-        .next()
-        .unwrap();
-    metas
-        .nested
-        .extend::<Punctuated<NestedMeta, Token![,]>>(extra_metas.into_iter().collect());
-    *attr = parse_quote!(#[#metas]);
-}
-fn extend_derivatives(ast: &mut DeriveInput, extra_derivatives: Vec<NestedMeta>) {
-    extend_metas(ast, extra_derivatives, "derivative");
-}
-fn extend_derives(ast: &mut DeriveInput, extra_derives: Vec<NestedMeta>) {
-    extend_metas(ast, extra_derives, "derive");
-}
-fn aorist_ast(args: TokenStream, input: TokenStream, derives: Vec<NestedMeta>) -> TokenStream {
-    let input_attrs = parse_macro_input!(args as AttributeArgs);
-    let (mut extra_derives, extra_derivatives) = get_derives(input_attrs);
-    for derive in derives {
-        extra_derives.push(derive);
-    }
-    let mut ast = parse_macro_input!(input as DeriveInput);
-    let quoted2 = match &mut ast.data {
-        syn::Data::Struct(ref mut struct_data) => {
-            add_aorist_fields(struct_data);
-            let quoted = quote! {
-                #[derive(
-                    Derivative, Serialize, Deserialize, Clone,
-                )]
-                #[derivative(PartialEq, Debug, Eq)]
-                #ast
-            };
-            let mut final_ast: DeriveInput = syn::parse2(quoted).unwrap();
-
-            extend_derivatives(&mut final_ast, extra_derivatives);
-            extend_derives(&mut final_ast, extra_derives);
-
-            quote! { #final_ast }
-        }
-        Data::Enum(DataEnum { variants, .. }) => {
-            let enum_name = &ast.ident;
-            let variant = variants.iter().map(|x| (&x.ident)).collect::<Vec<_>>();
-            let quoted = quote! {
-                #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
-                #[serde(tag = "type")]
-                pub enum #enum_name {
-                    #(#variant(#variant)),*
+struct ConceptBuilder {}
+impl ConceptBuilder {
+    fn get_derives(attrs: Vec<NestedMeta>) -> (Vec<NestedMeta>, Vec<NestedMeta>) {
+        let mut derivatives: Vec<NestedMeta> = Vec::new();
+        let mut derives: Vec<NestedMeta> = Vec::new();
+        for attr in attrs {
+            if let NestedMeta::Meta(Meta::List(x)) = attr {
+                if x.path.is_ident("derivative") {
+                    derivatives = x.nested.into_iter().collect();
+                } else if x.path.is_ident("derive") {
+                    derives = x.nested.into_iter().collect();
+                } else {
+                    panic!("An attribute other than derive or derivative was specified");
                 }
-            };
-            let mut final_ast: DeriveInput = syn::parse2(quoted).unwrap();
-            extend_derives(&mut final_ast, extra_derives);
-            extend_derives(&mut final_ast, extra_derivatives);
+            } else {
+                panic!("An attribute other than a MetaList was specified.");
+            }
+        }
+        (derives, derivatives)
+    }
 
-            quote! {
-                #final_ast
-                impl #enum_name {
-                    pub fn is_same_variant_in_enum_as(&self, other: &Self) -> bool {
-                        match (self, other) {
-                            #(
-                                (#enum_name::#variant(_), #enum_name::#variant(_)) => true,
-                            )*
-                            _ => false,
+    fn add_aorist_fields(struct_data: &mut syn::DataStruct) {
+        match &mut struct_data.fields {
+            Fields::Named(fields) => {
+                fields.named.push(
+                    Field::parse_named
+                        .parse2(quote! {
+                        pub uuid: Option<Uuid>
+                        })
+                        .unwrap(),
+                );
+                fields.named.push(
+                    Field::parse_named
+                        .parse2(quote! {
+                        pub tag: Option<String>
+                        })
+                        .unwrap(),
+                );
+                fields.named.push(
+                    Field::parse_named
+                        .parse2(quote! {
+                            #[serde(skip)]
+                            #[derivative(PartialEq = "ignore", Debug = "ignore", Hash = "ignore")]
+                            pub constraints: Vec<Arc<RwLock<Constraint>>>
+                        })
+                        .unwrap(),
+                );
+            }
+            _ => (),
+        }
+    }
+    fn extend_metas(ast: &mut DeriveInput, extra_metas: Vec<NestedMeta>, ident: &str) {
+        let (attr, mut metas) = ast
+            .attrs
+            .iter_mut()
+            .filter(|attr| attr.path.is_ident(ident))
+            .filter_map(|attr| match attr.parse_meta() {
+                Ok(Meta::List(meta_list)) => Some((attr, meta_list)),
+                _ => None, // kcov-ignore
+            })
+            .next()
+            .unwrap();
+        metas
+            .nested
+            .extend::<Punctuated<NestedMeta, Token![,]>>(extra_metas.into_iter().collect());
+        *attr = parse_quote!(#[#metas]);
+    }
+    fn extend_derivatives(ast: &mut DeriveInput, extra_derivatives: Vec<NestedMeta>) {
+        Self::extend_metas(ast, extra_derivatives, "derivative");
+    }
+    fn extend_derives(ast: &mut DeriveInput, extra_derives: Vec<NestedMeta>) {
+        Self::extend_metas(ast, extra_derives, "derive");
+    }
+
+    fn gen(args: TokenStream, input: TokenStream, derives: Vec<NestedMeta>) -> TokenStream {
+        let input_attrs = parse_macro_input!(args as AttributeArgs);
+        let (mut extra_derives, extra_derivatives) = Self::get_derives(input_attrs);
+        for derive in derives {
+            extra_derives.push(derive);
+        }
+        let mut ast = parse_macro_input!(input as DeriveInput);
+        let quoted2 = match &mut ast.data {
+            syn::Data::Struct(ref mut struct_data) => {
+                Self::add_aorist_fields(struct_data);
+                let quoted = quote! {
+                    #[derive(
+                        Derivative, Serialize, Deserialize, Clone,
+                    )]
+                    #[derivative(PartialEq, Debug, Eq)]
+                    #ast
+                };
+                let mut final_ast: DeriveInput = syn::parse2(quoted).unwrap();
+
+                Self::extend_derivatives(&mut final_ast, extra_derivatives);
+                Self::extend_derives(&mut final_ast, extra_derives);
+
+                quote! { #final_ast }
+            }
+            Data::Enum(DataEnum { variants, .. }) => {
+                let enum_name = &ast.ident;
+                let variant = variants.iter().map(|x| (&x.ident)).collect::<Vec<_>>();
+                let quoted = quote! {
+                    #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+                    #[serde(tag = "type")]
+                    pub enum #enum_name {
+                        #(#variant(#variant)),*
+                    }
+                };
+                let mut final_ast: DeriveInput = syn::parse2(quoted).unwrap();
+                Self::extend_derives(&mut final_ast, extra_derives);
+                Self::extend_derives(&mut final_ast, extra_derivatives);
+
+                quote! {
+                    #final_ast
+                    impl #enum_name {
+                        pub fn is_same_variant_in_enum_as(&self, other: &Self) -> bool {
+                            match (self, other) {
+                                #(
+                                    (#enum_name::#variant(_), #enum_name::#variant(_)) => true,
+                                )*
+                                _ => false,
+                            }
                         }
                     }
                 }
             }
-        }
-        _ => panic!("expected a struct with named fields or an enum"),
-    };
-    proc_macro::TokenStream::from(quoted2)
+            _ => panic!("expected a struct with named fields or an enum"),
+        };
+        proc_macro::TokenStream::from(quoted2)
+    }
 }
 
 #[proc_macro_attribute]
@@ -232,9 +235,9 @@ pub fn aorist_concept(args: TokenStream, input: TokenStream) -> TokenStream {
         let derive = NestedMeta::Meta(Meta::Path(path));
         extra_derives.push(derive);
     }
-    aorist_ast(args, input, extra_derives)
+    ConceptBuilder::gen(args, input, extra_derives)
 }
 #[proc_macro_attribute]
 pub fn aorist(args: TokenStream, input: TokenStream) -> TokenStream {
-    aorist_ast(args, input, vec![])
+    ConceptBuilder::gen(args, input, vec![])
 }
