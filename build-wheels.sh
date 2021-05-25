4a1595e448d8367b43c061d2ff97ded8c20d4a71
Hoang Nguyen <nvthoang@gmail.com>
2021-05-25 17:42:50 +1200

    create tar.bz2 file but the file failed to work

--- build-wheels.sh
+++ build-wheels.sh
@@ -8,6 +8,8 @@ export PATH="$HOME/.cargo/bin:$PATH"
 for PYBIN in /opt/python/cp{36,37,38,39}*/bin; do
     "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
         "${PYBIN}/python" setup.py bdist_wheel
+        "${PYBIN}/python" setup.py sdist --formats=bztar
+        
         done
 
         for whl in dist/*.whl; do
