708f2cb5d8a75bdc57a067a74098b4fa9fc2a245
Hoang Nguyen <nvthoang@gmail.com>
2021-05-31 14:35:20 +1200

    working for bdist (setuptools-rust) and sdist (maturin)

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -ex
 
-cd /io
-
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
 export PATH="$HOME/.cargo/bin:$PATH"
+
+cd /io
 
 for PYBIN in /opt/python/cp{36,37,38,39}*/bin; do
     "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
         "${PYBIN}/python" setup.py bdist_wheel
-        "${PYBIN}/python" setup.py sdist --formats=bztar #gztar
-        
         done
 
         for whl in dist/*.whl; do
