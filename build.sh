79db643d644f85b8192ca96a80cc6dd40992f456
Hoang Nguyen <nvthoang@gmail.com>
2021-06-01 15:47:07 +1200

    working conda-build recipe for wheel file

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#conda config --add channels conda-forge
+#conda install maturin
+#pip install /home/hoang/nvth_programing/aorist-test/target/wheels/aorist-0.0.1.tar.gz
+#ls
+#$PYTHON setup.py install
+pip install /home/hoang/nvth_programing/aorist-test/dist/aorist-0.0.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl 
