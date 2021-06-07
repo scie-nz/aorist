c9f0270af12a2c82ef261c77568ce7cc3f2ae21b
Hoang Nguyen <nvthoang@gmail.com>
2021-06-08 00:49:52 +1200

    replaced with relative link

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 #pip install /home/hoang/nvth_programing/aorist-test/target/wheels/aorist-0.0.1.tar.gz
 #ls
 #$PYTHON setup.py install
-pip install /home/hoang/nvth_programing/aorist-test/dist/aorist-0.0.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl 
+pip install dist/aorist-0.0.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl 
