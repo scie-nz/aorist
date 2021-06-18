4ed85443a4ebc1cfb9f27c47e6252083442ace4e
Bogdan State <bogdan@scie.nz>
2021-06-18 20:59:14 +1200

    Added measure submodule to aorist_core template

--- aorist_core/src/template/mod.rs
+++ aorist_core/src/template/mod.rs
@@ -1,9 +1,11 @@
 mod datum_template;
 mod filter;
 mod identifier_tuple;
+mod measure;
 mod row_struct;
 
 pub use datum_template::*;
 pub use filter::*;
 pub use identifier_tuple::*;
+pub use measure::*;
 pub use row_struct::*;
