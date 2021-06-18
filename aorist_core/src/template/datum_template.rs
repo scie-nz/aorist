a219b16481008f75045a861125a9bc2a1c300484
Bogdan State <bogdan@scie.nz>
2021-06-18 21:04:32 +1200

    Added DatumTemplate struct

--- aorist_core/src/template/datum_template.rs
+++ aorist_core/src/template/datum_template.rs
@@ -1,6 +1,62 @@
 use aorist_attributes::Attribute;
+use aorist_primitives::{AoristConcept, ConceptEnum};
+use crate::template::measure::*;
+use crate::template::row_struct::RowStruct;
+use crate::template::filter::Filter;
+use crate::template::identifier_tuple::IdentifierTuple;
+use aorist_concept::{aorist, Constrainable};
+use paste::paste;
+use uuid::Uuid;
+use serde::{Deserialize, Serialize};
 
 pub trait TDatumTemplate {
     fn get_attributes(&self) -> Vec<Attribute>;
     fn get_name(&self) -> String;
+}
+
+#[aorist]
+pub enum DatumTemplate {
+    TrainedFloatMeasure(TrainedFloatMeasure),
+    PredictionsFromTrainedFloatMeasure(PredictionsFromTrainedFloatMeasure),
+    RowStruct(RowStruct),
+    IdentifierTuple(IdentifierTuple),
+    IntegerMeasure(IntegerMeasure),
+    Filter(Filter),
+}
+impl DatumTemplate {
+    pub fn get_type(&self) -> String {
+        match self {
+            DatumTemplate::RowStruct(_) => "RowStruct",
+            DatumTemplate::IdentifierTuple(_) => "IdentifierTuple",
+            DatumTemplate::IntegerMeasure(_) => "IntegerMeasure",
+            DatumTemplate::TrainedFloatMeasure(_) => "TrainedFloatMeasure",
+            DatumTemplate::PredictionsFromTrainedFloatMeasure(_) => {
+                "PredictionsFromTrainedFloatMeasure"
+            }
+            DatumTemplate::Filter(_) => "Filter",
+        }
+        .to_string()
+    }
+}
+impl TDatumTemplate for DatumTemplate {
+    fn get_name(&self) -> String {
+        match self {
+            DatumTemplate::RowStruct(x) => x.get_name(),
+            DatumTemplate::IdentifierTuple(x) => x.get_name(),
+            DatumTemplate::IntegerMeasure(x) => x.get_name(),
+            DatumTemplate::TrainedFloatMeasure(x) => x.get_name(),
+            DatumTemplate::PredictionsFromTrainedFloatMeasure(x) => x.get_name(),
+            DatumTemplate::Filter(x) => x.get_name(),
+        }
+    }
+    fn get_attributes(&self) -> Vec<Attribute> {
+        match self {
+            DatumTemplate::RowStruct(x) => x.get_attributes(),
+            DatumTemplate::IdentifierTuple(x) => x.get_attributes(),
+            DatumTemplate::IntegerMeasure(x) => x.get_attributes(),
+            DatumTemplate::TrainedFloatMeasure(x) => x.get_attributes(),
+            DatumTemplate::PredictionsFromTrainedFloatMeasure(x) => x.get_attributes(),
+            DatumTemplate::Filter(x) => x.get_attributes(),
+        }
+    }
 }
