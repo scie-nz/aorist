5eaebc77c0332ea006f452622cad7edae1dd9324
Bogdan State <bogdan@scie.nz>
2021-06-20 18:15:43 +1200

    Added process_constraint_block to driver

--- aorist_core/src/driver.rs
+++ aorist_core/src/driver.rs
@@ -119,4 +119,161 @@ where
 
         sorted_builders
     }
+    fn init_unsatisfied_constraints(&self) -> Result<ConstraintsBlockMap<'a, 'b, C>>;
+
+    fn find_satisfiable_constraint_block(
+        &self,
+        unsatisfied_constraints: &mut ConstraintsBlockMap<'a, 'b, C>,
+    ) -> Option<(
+        LinkedHashMap<(Uuid, String), Arc<RwLock<ConstraintState<'a, 'b, C>>>>,
+        String,
+    )> {
+        let constraint_block_name = unsatisfied_constraints
+            .iter()
+            .filter(|(_, v)| v.0.len() == 0)
+            .map(|(k, _)| k.clone())
+            .next();
+        match constraint_block_name {
+            Some(name) => {
+                let (_dependency_names, constraints) =
+                    unsatisfied_constraints.remove(&name).unwrap();
+                for (_, (v, _)) in unsatisfied_constraints.iter_mut() {
+                    v.remove(&name);
+                }
+                Some((constraints, name))
+            }
+            None => None,
+        }
+    }
+    fn init_tasks_dict(
+        block: &LinkedHashMap<(Uuid, String), Arc<RwLock<ConstraintState<'a, 'b, C>>>>,
+        constraint_name: String,
+    ) -> Option<AST> {
+        match block.len() == 1 {
+            true => None,
+            false => Some(AST::SimpleIdentifier(SimpleIdentifier::new_wrapped(
+                format!("tasks_{}", constraint_name).to_string(),
+            ))),
+        }
+    }
+    fn get_constraint_rwlock(&self, uuid: &(Uuid, String)) -> Arc<RwLock<C>>;
+    fn get_preferences(&self) -> Vec<Dialect>;
+    fn get_ancestry(&self) -> Arc<<C as OuterConstraint<'a, 'b>>::TAncestry>;
+    fn process_constraint_with_program(
+        &mut self,
+        constraint: RwLockReadGuard<'_, C>,
+        uuid: (Uuid, String),
+        calls: &mut HashMap<(String, String, String), Vec<(String, ParameterTuple)>>,
+        state: Arc<RwLock<ConstraintState<'a, 'b, C>>>,
+    ) {
+        let name = constraint.get_name().clone();
+        drop(constraint);
+        // TODO: turn into a reference to a field on self
+        let preferences = self.get_preferences();
+        let mut write = state.write().unwrap();
+        // TODO: remove dummy hash map
+        write.satisfy(&preferences, self.get_ancestry());
+        drop(write);
+
+        // TODO: preambles and calls are superflous
+        let key = state.read().unwrap().key.as_ref().unwrap().clone();
+        calls
+            .entry((
+                state.read().unwrap().get_call().unwrap(),
+                name,
+                uuid.1.clone(),
+            ))
+            .or_insert(Vec::new())
+            .push((key, state.read().unwrap().get_params().unwrap()));
+    }
+    fn process_constraint_state(
+        &mut self,
+        uuid: (Uuid, String),
+        state: Arc<RwLock<ConstraintState<'a, 'b, C>>>,
+        calls: &mut HashMap<(String, String, String), Vec<(String, ParameterTuple)>>,
+        reverse_dependencies: &HashMap<(Uuid, String), HashSet<(String, Uuid, String)>>,
+        unsatisfied_constraints: &ConstraintsBlockMap<'a, 'b, C>,
+    ) -> Result<()> {
+        let read = state.read().unwrap();
+        assert!(!read.satisfied);
+        assert_eq!(read.unsatisfied_dependencies.len(), 0);
+        drop(read);
+
+        let rw = self.get_constraint_rwlock(&uuid);
+        let constraint = rw.read().unwrap();
+
+        if constraint.requires_program()? {
+            self.process_constraint_with_program(constraint, uuid.clone(), calls, state.clone());
+        }
+
+        if let Some(v) = reverse_dependencies.get(&uuid) {
+            for (dependency_name, dependency_uuid, dependency_root_type) in v {
+                let rw = unsatisfied_constraints
+                    .get(dependency_name)
+                    .unwrap()
+                    .1
+                    .get(&(*dependency_uuid, dependency_root_type.clone()))
+                    .unwrap();
+                let mut write = rw.write().unwrap();
+                write.satisfied_dependencies.push(state.clone());
+                assert!(write.unsatisfied_dependencies.remove(&uuid));
+                drop(write);
+            }
+        }
+
+        let mut write = state.write().unwrap();
+        write.satisfied = true;
+        drop(write);
+        Ok(())
+    }
+    fn mark_constraint_state_as_satisfied(
+        &mut self,
+        id: (Uuid, String),
+        state: Arc<RwLock<ConstraintState<'a, 'b, C>>>,
+    );
+    fn process_constraint_block(
+        &mut self,
+        block: &mut LinkedHashMap<(Uuid, String), Arc<RwLock<ConstraintState<'a, 'b, C>>>>,
+        reverse_dependencies: &HashMap<(Uuid, String), HashSet<(String, Uuid, String)>>,
+        constraint_name: String,
+        unsatisfied_constraints: &ConstraintsBlockMap<'a, 'b, C>,
+        identifiers: &HashMap<Uuid, AST>,
+    ) -> Result<(
+        Vec<<Self::CB as ConstraintBlock<'a, 'b, <D as FlowBuilderBase>::T, C>>::C>,
+        Option<AST>,
+    )> {
+        let tasks_dict = Self::init_tasks_dict(block, constraint_name.clone());
+        // (call, constraint_name, root_name) => (uuid, call parameters)
+        let mut calls: HashMap<(String, String, String), Vec<(String, ParameterTuple)>> =
+            HashMap::new();
+        let mut blocks = Vec::new();
+        let mut by_dialect: HashMap<Option<Dialect>, Vec<Arc<RwLock<ConstraintState<'a, 'b, C>>>>> =
+            HashMap::new();
+        for (id, state) in block.clone() {
+            self.process_constraint_state(
+                id.clone(),
+                state.clone(),
+                &mut calls,
+                reverse_dependencies,
+                unsatisfied_constraints,
+            )?;
+            self.mark_constraint_state_as_satisfied(id, state.clone());
+            by_dialect
+                .entry(state.read().unwrap().get_dialect())
+                .or_insert(Vec::new())
+                .push(state.clone());
+        }
+        for (_dialect, satisfied) in by_dialect.into_iter() {
+            let block = <Self::CB as ConstraintBlock<'a, 'b, <D as FlowBuilderBase>::T, C>>::C::new(
+                satisfied,
+                constraint_name.clone(),
+                tasks_dict.clone(),
+                identifiers,
+            )?;
+            blocks.push(block);
+        }
+
+        Ok((blocks, tasks_dict))
+    }
+
 }
