56cb28087a5e3fec8c53ea5315d17479b5ff59fd
Bogdan State <bogdan@scie.nz>
2021-06-21 21:19:59 +1200

    Full concept enum back

--- aorist_core/src/concept/mod.rs
+++ aorist_core/src/concept/mod.rs
@@ -1,4 +1,4 @@
-// pub use crate::universe::*;
+pub use crate::universe::*;
 use aorist_concept::{aorist, Constrainable};
 pub use aorist_primitives::{
     register_concept, Ancestry, AoristConcept, ConceptEnum, TConceptEnum,
@@ -59,126 +59,125 @@ impl <T: Debug + Clone + Serialize + PartialEq> Hash for AoristRef<T> where T: H
     }
 }
 
-// use crate::access_policy::*;
-// use crate::algorithms::*;
-// use crate::asset::*;
-// use crate::attributes::*;
-// use crate::compliance::*;
-// use crate::compression::*;
-// use crate::dataset::*;
-// use crate::encoding::*;
-// use crate::endpoints::*;
-// use crate::header::*;
-// use crate::layout::*;
-// use crate::location::*;
-// use crate::models::*;
-// use crate::predicate::*;
+use crate::access_policy::*;
+use crate::algorithms::*;
+use crate::asset::*;
+use crate::attributes::*;
+use crate::compliance::*;
+use crate::compression::*;
+use crate::dataset::*;
+use crate::encoding::*;
+use crate::endpoints::*;
+use crate::header::*;
+use crate::layout::*;
+use crate::location::*;
+use crate::models::*;
+use crate::predicate::*;
 use crate::role::*;
-// use crate::role_binding::*;
-// use crate::schema::*;
-// use crate::storage::*;
-// use crate::storage_setup::*;
-// use crate::template::*;
+use crate::role_binding::*;
+use crate::schema::*;
+use crate::storage::*;
+use crate::storage_setup::*;
+use crate::template::*;
 use crate::user::*;
-// use crate::user_group::*;
+use crate::user_group::*;
 // pub struct WrappedConcept<'a, T> {
 //     pub inner: T,
 //     pub _phantom_lt: std::marker::PhantomData<&'a ()>,
 // }
 //
-register_concept!(Concept, ConceptAncestry, GlobalPermissionsAdmin, Role, User);
-// register_concept!(
-//     Concept,
-//     ConceptAncestry,
-//     AccessPolicy,
-//     ApproveAccessSelector,
-//     RegressionAlgorithm,
-//     RandomForestRegressionAlgorithm,
-//     SVMRegressionAlgorithm,
-//     Asset,
-//     StaticDataTable,
-//     SupervisedModel,
-//     DerivedAsset,
-//     Attribute,
-//     Predicate,
-//     APILayout,
-//     APIOrFileLayout,
-//     FileBasedStorageLayout,
-//     SingleFileLayout,
-//     PushshiftSubredditPostsAPILayout,
-//     TabularLayout,
-//     DynamicTabularLayout,
-//     StaticTabularLayout,
-//     Granularity,
-//     DailyGranularity,
-//     DataSet,
-//     Role,
-//     GlobalPermissionsAdmin,
-//     GzipCompression,
-//     DataCompression,
-//     ZipCompression,
-//     ComplianceConfig,
-//     CSVHeader,
-//     FileHeader,
-//     AlluxioLocation,
-//     BigQueryLocation,
-//     GCSLocation,
-//     GithubLocation,
-//     HiveLocation,
-//     LocalFileSystemLocation,
-//     OnPremiseLocation,
-//     MinioLocation,
-//     S3Location,
-//     PostgresLocation,
-//     PushshiftAPILocation,
-//     RemoteLocation,
-//     SQLiteLocation,
-//     WebLocation,
-//     Model,
-//     SingleObjectiveRegressor,
-//     GDBEncoding,
-//     CSVEncoding,
-//     TSVEncoding,
-//     Encoding,
-//     JSONEncoding,
-//     NewlineDelimitedJSONEncoding,
-//     ORCEncoding,
-//     ONNXEncoding,
-//     UndefinedTabularSchema,
-//     TabularSchema,
-//     TimeOrderedTabularSchema,
-//     DataSchema,
-//     Universe,
-//     LocalStorageSetup,
-//     RemoteStorageSetup,
-//     ReplicationStorageSetup,
-//     ComputedFromLocalData,
-//     StorageSetup,
-//     Storage,
-//     BigQueryStorage,
-//     SQLiteStorage,
-//     HiveTableStorage,
-//     RemoteStorage,
-//     LocalFileStorage,
-//     PostgresStorage,
-//     GitStorage,
-//     RoleBinding,
-//     DatumTemplate,
-//     IdentifierTuple,
-//     RowStruct,
-//     IntegerMeasure,
-//     Filter,
-//     User,
-//     UserGroup,
-//     EndpointConfig,
-//     AWSConfig,
-//     GCPConfig,
-//     GiteaConfig,
-//     PostgresConfig,
-//     AlluxioConfig,
-//     RangerConfig,
-//     PrestoConfig,
-//     MinioConfig,
-//     TrainedFloatMeasure,
-//     PredictionsFromTrainedFloatMeasure
-// );
+register_concept!(
+    Concept,
+    ConceptAncestry,
+    AccessPolicy,
+    ApproveAccessSelector,
+    RegressionAlgorithm,
+    RandomForestRegressionAlgorithm,
+    SVMRegressionAlgorithm,
+    Asset,
+    StaticDataTable,
+    SupervisedModel,
+    DerivedAsset,
+    Attribute,
+    Predicate,
+    APILayout,
+    APIOrFileLayout,
+    FileBasedStorageLayout,
+    SingleFileLayout,
+    PushshiftSubredditPostsAPILayout,
+    TabularLayout,
+    DynamicTabularLayout,
+    StaticTabularLayout,
+    Granularity,
+    DailyGranularity,
+    DataSet,
+    Role,
+    GlobalPermissionsAdmin,
+    GzipCompression,
+    DataCompression,
+    ZipCompression,
+    ComplianceConfig,
+    CSVHeader,
+    FileHeader,
+    AlluxioLocation,
+    BigQueryLocation,
+    GCSLocation,
+    GithubLocation,
+    HiveLocation,
+    LocalFileSystemLocation,
+    OnPremiseLocation,
+    MinioLocation,
+    S3Location,
+    PostgresLocation,
+    PushshiftAPILocation,
+    RemoteLocation,
+    SQLiteLocation,
+    WebLocation,
+    Model,
+    SingleObjectiveRegressor,
+    GDBEncoding,
+    CSVEncoding,
+    TSVEncoding,
+    Encoding,
+    JSONEncoding,
+    NewlineDelimitedJSONEncoding,
+    ORCEncoding,
+    ONNXEncoding,
+    UndefinedTabularSchema,
+    TabularSchema,
+    TimeOrderedTabularSchema,
+    DataSchema,
+    Universe,
+    LocalStorageSetup,
+    RemoteStorageSetup,
+    ReplicationStorageSetup,
+    ComputedFromLocalData,
+    StorageSetup,
+    Storage,
+    BigQueryStorage,
+    SQLiteStorage,
+    HiveTableStorage,
+    RemoteStorage,
+    LocalFileStorage,
+    PostgresStorage,
+    GitStorage,
+    RoleBinding,
+    DatumTemplate,
+    IdentifierTuple,
+    RowStruct,
+    IntegerMeasure,
+    Filter,
+    User,
+    UserGroup,
+    EndpointConfig,
+    AWSConfig,
+    GCPConfig,
+    GiteaConfig,
+    PostgresConfig,
+    AlluxioConfig,
+    RangerConfig,
+    PrestoConfig,
+    MinioConfig,
+    TrainedFloatMeasure,
+    PredictionsFromTrainedFloatMeasure
+);
