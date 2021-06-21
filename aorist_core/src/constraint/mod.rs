b714a806216a1d68f5d320691c1f4f770f05252a
Bogdan State <bogdan@scie.nz>
2021-06-21 22:21:53 +1200

    Removed lifetimes from aorist_core constraint

--- aorist_core/src/constraint/mod.rs
+++ aorist_core/src/constraint/mod.rs
@@ -3,35 +3,31 @@ use crate::parameter_tuple::ParameterTuple;
 use anyhow::Result;
 use aorist_primitives::{Ancestry, OuterConstraint, TConstraint};
 use std::sync::Arc;
-/*
-pub trait SatisfiableConstraint<'a, 'b>: TConstraint<'a, 'b>
-where
-    'a: 'b,
+
+pub trait SatisfiableConstraint<'a>: TConstraint<'a>
 {
-    type TAncestry: Ancestry<'a>;
+    type TAncestry: Ancestry;
     fn satisfy(
         &mut self,
-        c: <Self::TAncestry as Ancestry<'a>>::TConcept,
+        c: <Self::TAncestry as Ancestry>::TConcept,
         d: &Dialect,
         ancestry: Arc<Self::TAncestry>,
     ) -> Result<Option<(String, String, ParameterTuple, Dialect)>>;
 
     fn satisfy_given_preference_ordering(
         &mut self,
-        r: <Self::TAncestry as Ancestry<'a>>::TConcept,
+        r: <Self::TAncestry as Ancestry>::TConcept,
         preferences: &Vec<Dialect>,
         ancestry: Arc<Self::TAncestry>,
     ) -> Result<(String, String, ParameterTuple, Dialect)>;
 }
 // TODO: duplicate function, should be unified in trait
-pub trait SatisfiableOuterConstraint<'a, 'b>: OuterConstraint<'a, 'b>
-where
-    'a: 'b,
+pub trait SatisfiableOuterConstraint<'a>: OuterConstraint<'a>
 {
     fn satisfy_given_preference_ordering(
         &mut self,
-        c: <<Self as OuterConstraint<'a, 'b>>::TAncestry as Ancestry<'a>>::TConcept,
+        c: <<Self as OuterConstraint<'a>>::TAncestry as Ancestry>::TConcept,
         preferences: &Vec<Dialect>,
-        ancestry: Arc<<Self as OuterConstraint<'a, 'b>>::TAncestry>,
+        ancestry: Arc<<Self as OuterConstraint<'a>>::TAncestry>,
     ) -> Result<(String, String, ParameterTuple, Dialect)>;
-}*/
+}
