79c42bd539644c087d9ed27328c667ad57e5d3a4
Bogdan State <bogdan@scie.nz>
2021-06-21 19:36:22 +1200

    compression module live again

--- aorist_core/src/compression/data_compression.rs
+++ aorist_core/src/compression/data_compression.rs
@@ -1,15 +1,16 @@
 use crate::compression::gzip_compression::*;
 use crate::compression::zip_compression::*;
-use crate::{AoristConcept, ConceptEnum};
+use crate::{AoristConcept, AoristRef, WrappedConcept, ConceptEnum};
 use aorist_concept::{aorist, Constrainable};
 use paste::paste;
+use std::fmt::Debug;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[aorist]
 pub enum DataCompression {
     #[constrainable]
-    GzipCompression(GzipCompression),
+    GzipCompression(AoristRef<GzipCompression>),
     #[constrainable]
-    ZipCompression(ZipCompression),
+    ZipCompression(AoristRef<ZipCompression>),
 }
