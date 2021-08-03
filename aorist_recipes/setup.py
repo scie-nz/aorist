8499d0341852b2c11860f438eff7a11b32c1a9e3
Bogdan State <bogdan@scie.nz>
2021-08-04 11:43:47 +1200

    Conda build artifacts for aorist_recipes

---
+++ aorist_recipes/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="aorist_recipes",
+    version="0.0.1",
+    packages=["aorist_recipes"],
+    zip_safe=True,
+    long_description="""
+    Recipes for aorist package.
+    """,
+    long_description_content_type="text/x-rst"
+)
+
