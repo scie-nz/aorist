a6c3cacf364b4b13d8fe068756a0e4f37dcdc768
Bogdan State <bogdan@scie.nz>
2021-08-09 00:47:09 +1200

    Test works on OSX

---
+++ upload_osx.sh
@@ -0,0 +1,4 @@
+anaconda upload \
+    /Users/bogdan/anaconda3/conda-bld/osx-64/aorist-0.0.1-py38_1.tar.bz2 \
+    /Users/bogdan/anaconda3/conda-bld/noarch/aorist_recipes-0.0.1-py_0.tar.bz2 \
+    /Users/bogdan/anaconda3/conda-bld/noarch/scienz-0.0.1-py_0.tar.bz2 --force
