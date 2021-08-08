2e3704ac3f5677228dbbbe4f67dc79a2d0d81856
Bogdan State <bogdan@scie.nz>
2021-08-08 15:03:36 +1200

    Conda recipe no longer dependent on cargo

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-$PYTHON setup.py install
-
-# Working BUT using absolute path
-
---
+++ clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd aorist_util && cargo clean && cd ..
+cd aorist_derive && cargo clean && cd ..
+cd aorist_primitives && cargo clean && cd ..
+cd aorist_concept && cargo clean && cd ..
+cd aorist_ast && cargo clean && cd ..
+cd aorist_attributes && cargo clean && cd ..
+cd aorist_constraint && cargo clean && cd ..
+cd aorist_core && cargo clean && cd ..
