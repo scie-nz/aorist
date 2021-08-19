89e37d8739d925e131e690d2524867618f07daba
Bogdan State <bogdan@scie.nz>
2021-08-19 17:38:16 +1200

    Deduping text attribute in SQLite where relevant

--- aorist_recipes/aorist_recipes/fasttext_training_data_from_sqlite.py
+++ aorist_recipes/aorist_recipes/fasttext_training_data_from_sqlite.py
@@ -12,6 +12,11 @@ programs = {}
         "source_tables": lambda fasttext_embedding: dumps([
             x.name() for x in fasttext_embedding.source_assets
         ]),
+        "dedup_text_attribute": lambda fasttext_embedding_schema: dumps(
+            fasttext_embedding_schema.text_attribute_name in fasttext_embedding_schema.source_schema.long_tabular_schema.key_attributes
+            if fasttext_embedding_schema.source_schema.long_tabular_schema is not None
+            else False
+        ),
         "db_filename": lambda fasttext_embedding: fasttext_embedding.setup.local_storage_setup.local.sq_lite_storage.location.file_name,
         "fasttext_training_data_file": lambda fasttext_embedding, context: (
             context.capture(
@@ -24,20 +29,25 @@ programs = {}
     },
 )
 def recipe(
-    text_attribute_name, source_tables, fasttext_training_data_file, db_filename, tmp_dir,
+    text_attribute_name, source_tables, fasttext_training_data_file, db_filename, tmp_dir, dedup_text_attribute,
 ):
     import sqlite3
     import json
 
-    def download_text_data_from_sqlite(text_attribute_name, source_tables, fasttext_training_data_file, db_filename, tmp_dir):
+    def download_text_data_from_sqlite(
+        text_attribute_name, source_tables, fasttext_training_data_file, db_filename, tmp_dir, dedup_text_attribute,
+    ):
      
+        dedup_text_attribute = json.loads(dedup_text_attribute)
+
         if not os.path.exists(tmp_dir):
             os.makedirs(tmp_dir)
         con = sqlite3.connect(db_filename)
         cursor = con.execute((chr(10) + "UNION ALL" + chr(10)).join([
-            "SELECT {text_attribute_name} FROM {source_table}".format(
+            "SELECT {dedup}{text_attribute_name} FROM {source_table}".format(
                 text_attribute_name=text_attribute_name,
                 source_table=source_table,
+                dedup="DISTINCT " if dedup_text_attribute else "",
             )
             for source_table in json.loads(source_tables)
         ]))
