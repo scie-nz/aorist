d21bd07bc8c017e398713ea533b1e27bb5f0fff4
Nick Parker <nick@scie.nz>
2021-09-12 18:02:33 +1200

    Use stock debian with Python+R+Rust for build environment

--- docker/build-env/build.sh
+++ docker/build-env/build.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-docker build . -t scienz/aorist-build-env:latest
+set -euo pipefail
+IFS=$'\n\t'
+
+# ideally we'd just build amd64, but blocking drone from deploying on arm doesn't work.
+# so for now let's just build arm64 too
+PLATFORMS="linux/amd64,linux/arm64"
+
+# Get 7-character commit SHA (note: doesn't detect dirty commits)
+COMMIT_SHA=$(git rev-parse HEAD | cut -b 1-7)
+
+IMG="scienz/aorist-build-env:${COMMIT_SHA}"
+
+echo "Building $IMG for $PLATFORMS"
+docker buildx build --push --platform $PLATFORMS -f Dockerfile -t $IMG .
