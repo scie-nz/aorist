bbf62b1ffde95dfd6ba192cc1eb26a463c53d0d3
Bogdan State <bogdan@scie.nz>
2021-09-13 07:04:26 +0000

    Added attributes to PolygonSchema

--- aorist_core/src/schema/polygon_schema.rs
+++ aorist_core/src/schema/polygon_schema.rs
@@ -3,12 +3,19 @@ use crate::concept::{AoristConcept, AoristRef, ConceptEnum, WrappedConcept};
 use crate::template::*;
 use aorist_concept::{aorist, Constrainable};
 use aorist_paste::paste;
-use aorist_primitives::primary_schema;
+use aorist_primitives::{attribute, primary_schema};
 use derivative::Derivative;
 #[cfg(feature = "python")]
 use pyo3::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 use uuid::Uuid;
+use aorist_attributes::*;
 
-primary_schema! { name: PolygonSchema }
+primary_schema! { 
+    name: PolygonSchema, 
+    attributes:
+      id: KeyStringIdentifier("Polygon Identifier", false),
+      name: FreeText("Polygon name", true),
+      wkt: WKTString("WKT string", false)
+}
