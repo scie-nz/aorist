283d05f11e8e5b7b1650b37fbedd8a92612dbd08
Nick Parker <nick@scie.nz>
2021-09-14 18:38:21 +1200

    Clean up deploy script: use 'helm template | kubectl apply'

--- kube-build/deploy.sh
+++ kube-build/deploy.sh
@@ -12,27 +12,23 @@ err_report() {
 }
 trap 'err_report $LINENO' ERR
 
-RELEASE_NAME=$(basename $SCRIPT_DIR)
-
 # set namespace, then reset back to current afterwards
+# this allows us to apply across namespaces in a single 'apply' command, while still having an assigned default
 TARGET_NAMESPACE=walden
 ORIG_NAMESPACE=$(kubectl config view --minify --output 'jsonpath={..namespace}')
 if [ -z "$ORIG_NAMESPACE" ]; then
     ORIG_NAMESPACE=$TARGET_NAMESPACE
 fi
+
 reset_namespace() {
-    # if namespace doesn't exist, create it
-    kubectl create namespace walden --dry-run=client -o yaml | kubectl apply -f -
     echo "Switching back to namespace: $ORIG_NAMESPACE"
     kubectl config set-context --current --namespace=$ORIG_NAMESPACE
 }
 trap reset_namespace EXIT
+
 echo "Switching to namespace: $TARGET_NAMESPACE"
+# if namespace doesn't exist, create it
+kubectl create namespace $TARGET_NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
 kubectl config set-context --current --namespace=$TARGET_NAMESPACE
 
-# force upgrade to work, otherwise get 'Error: UPGRADE FAILED: "secrets" has no deployed releases'
-# might be fixed with https://github.com/helm/helm/pull/7653/
-kubectl delete secret sh.helm.release.v1.${RELEASE_NAME}.v1 --ignore-not-found
-# test with:
-#   helm template --debug $RELEASE_NAME $SCRIPT_DIR
-helm upgrade --install --debug $RELEASE_NAME $SCRIPT_DIR
+helm template -g $SCRIPT_DIR | kubectl apply -f -
