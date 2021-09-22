3bdf5fe9418b4913862e818f146ff17e549e859a
Bogdan State <bogdan@scie.nz>
2021-09-22 05:39:08 +0000

    Added additional attributes to TAOCrownHullSchema

--- aorist_core/src/schema/tao_crown_hull_schema.rs
+++ aorist_core/src/schema/tao_crown_hull_schema.rs
@@ -18,7 +18,10 @@ derived_schema! {
     sources: 
       - point_cloud: PointCloudAsset,
     attributes:
-      prefix: KeyStringIdentifier("File Prefix", false)
+      prefix: KeyStringIdentifier("File Prefix", false),
+      tao_id: KeyInt64Identifier("ID of TAO (unique in file)", false),
+      wkt: WKTString("WKT string of TAO boundary", false),
+      metrics: JSON("JSON map of metrics", false) 
     fields:
       hull_type: String,
       concavity: FloatValue,
