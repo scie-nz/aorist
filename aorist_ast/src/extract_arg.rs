c3865a95d62b703be86503bf5e487293725189ec
Bogdan State <bogdan@scie.nz>
2021-09-28 13:21:36 +1300

    Added dictionary as possible value for extract_arg

--- aorist_ast/src/extract_arg.rs
+++ aorist_ast/src/extract_arg.rs
@@ -1,8 +1,9 @@
-use crate::{BigIntLiteral, BooleanLiteral, FloatLiteral, List, None, StringLiteral, Tuple, AST};
+use crate::{BigIntLiteral, BooleanLiteral, FloatLiteral, List, None, StringLiteral, Tuple, AST, Dict};
 use aorist_primitives::Context;
+use linked_hash_map::LinkedHashMap;
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
-use pyo3::types::{PyList, PyTuple};
+use pyo3::types::{PyList, PyTuple, PyDict};
 
 pub fn extract_arg(arg: &PyAny) -> PyResult<AST> {
     if arg.is_none() {
@@ -36,6 +37,21 @@ pub fn extract_arg(arg: &PyAny) -> PyResult<AST> {
             .map(|x| extract_arg(x))
             .collect::<PyResult<Vec<AST>>>()?;
         Ok(AST::Tuple(Tuple::new_wrapped(v, false)))
+    } else if let Ok(extracted_dict) = arg.downcast::<PyDict>() {
+        let m = extracted_dict
+            .iter()
+            .map(|(k, v)| (
+                match k.extract::<String>() {
+                    Ok(key) => key.clone(),
+                    Err(err) => panic!("Dictionary keys should be string. Got {:?} instead:\n{:?}", k, err),
+                }, 
+                match extract_arg(v) {
+                    Ok(val) => val,
+                    Err(err) => panic!("Problem when extracting value for key: {:?}:\n{:?}", k, err),
+                }
+            ))
+            .collect::<LinkedHashMap<String, AST>>();
+        Ok(AST::Dict(Dict::new_wrapped(m)))
     } else {
         Err(PyValueError::new_err(
             "Object can be either string, boolean, int, or a list",
