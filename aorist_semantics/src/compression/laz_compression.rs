6ffa712218d99db19d57d1bccbcf482500d29f6b
Bogdan State <bogdan@scie.nz>
2022-01-03 10:44:07 +1300

    Working minimal aorist_semantics (sort of)

---
+++ aorist_semantics/src/compression/laz_compression.rs
@@ -0,0 +1,13 @@
+use abi_stable::std_types::ROption;
+use aorist_concept::{aorist, Constrainable};
+use aorist_paste::paste;
+use aorist_primitives::AOption;
+use aorist_primitives::AoristRef;
+use aorist_primitives::{AString, AVec, AoristConcept, AoristConceptBase, ConceptEnum};
+use derivative::Derivative;
+use serde::{Deserialize, Serialize};
+use std::fmt::Debug;
+use uuid::Uuid;
+
+#[aorist]
+pub struct LAZCompression {}
