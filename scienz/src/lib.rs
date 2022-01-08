b6e3c75c13607877f9def25c72572c8e6a32d1c3
Bogdan State <bogdan@scie.nz>
2022-01-08 14:38:28 +1300

    scienz module: layout + storage + storage_setup compiling

--- scienz/src/lib.rs
+++ scienz/src/lib.rs
@@ -2,9 +2,15 @@
 mod compression;
 mod encoding;
 mod header;
+mod layout;
 mod location;
+mod storage;
+mod storage_setup;
 
 pub use compression::*;
 pub use encoding::*;
 pub use header::*;
+pub use layout::*;
 pub use location::*;
+pub use storage::*;
+pub use storage_setup::*;
