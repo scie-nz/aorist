580cf244acf9c91240b0a606bc2779e1b9215613
Bogdan State <bogdan@scie.nz>
2022-01-08 15:13:48 +1300

    scienz module: removed dead code

--- scienz/src/algorithms/mod.rs
+++
@@ -1,3 +0,0 @@
-mod regression;
-
-pub use regression::*;
--- scienz/src/features/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod objective;
-
-pub use objective::{
-    CanBeContinuousObjective, CanBeContinuousRegressionObjective, CanBeRegressionObjective,
-    ContinuousObjective, ContinuousRegressionObjective, RegressionObjective,
-};
